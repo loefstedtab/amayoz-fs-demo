b1dd4e445b79e08d774e1c61a5444b5098ffed9f
Arthur Loefstedt <theloefstedtab@gmail.com>
2022-12-01 12:19:59 -0500

    render authors function

--- client/Main.js
+++ client/Main.js
@@ -1,15 +1,50 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
+
+// get => '/api/authors' => [{id,name}, {id,name}];
+//onMount gets data
 
 const Main = () => {
   const [count, setCount] = useState(0);
-  useEffect(()=> {
-    console.log("Main Component Mounted")
-  }, [])
-  return (
+  const [authors, setAuthors] = useState([])
+  useEffect(() => {
+    const getAuthors = async () => {
+      let response = await axios.get("/api/authors");
+      let data = response.data;
+      setAuthors(data)
+      console.log("response", authors);
+      console.log("Main Component Mounted");
+    };
+    getAuthors();
+  }, []);
+
+  const renderAuthors = () => {
+    if (!authors){
+      return(
+        <p>authors not loaded yet</p>
+      )
+    }
+    authors.map((a) => {
+      return (
+        <div className="comp">
+          <h1>{a.name}</h1>
+          <p>{a.id}</p>
+        </div>
+      )
+    })
+  }
+   return (
     <div id="main" className="row container">
       <h1>Amayoz</h1>
       <p>Count: {count}</p>
-      <p onClick={() => {setCount(count +1)}}>add</p>
+      <p
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        add
+      </p>
+     {renderAuthors()}
     </div>
   );
 };
